44a5ce8ea56c0b28b86da45858d7c72860d6972b
ChillibyteUK <dan@chillibyte.co.uk>
2026-08-06 11:01:42 +0100

    styled text image

--- blocks/cb-styled-text-image.php
+++ blocks/cb-styled-text-image.php
@@ -9,15 +9,8 @@ defined( 'ABSPATH' ) || exit;
 
 $block_id = $block['anchor'] ?? ( $block['id'] ?? wp_unique_id( 'cb-styled-text-image-' ) );
 
-$section_title = get_field( 'section_title' );
-$title_colour  = get_field( 'title_colour' );
 ?>
 <section id="<?= esc_attr( $block_id ); ?>" class="cb-styled-text-image">
-	<div class="cb-styled-text-image__pre-title <?= esc_attr( $title_colour ); ?>">
-		<div class="id-container pt-2 pb-1 px-4 px-md-5">
-			<?= esc_html( $section_title ); ?>
-		</div>
-	</div>
 	<div class="id-container ps-5">
 		<div class="row">
 			<div class="col-md-6 py-5 cb-styled-text-image__text-content">
